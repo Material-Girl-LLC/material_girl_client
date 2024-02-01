e5c6865da3097d01df6dfb5247e0d26e5da00db7
mcschwartzman <mcschwartzman@outlook.com>
2024-01-31 21:19:23 -0500

    fix: adds material admin page to list/delete materials

--- src/components/MaterialsAdmin.js
+++ src/components/MaterialsAdmin.js
@@ -1,13 +1,73 @@
-import React from "react";
+import React, {Component} from "react";
 import { Row, Col, Container } from 'react-bootstrap';
 import Homebar from './Homebar';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
-function MaterialsAdmin() {
+class MaterialsAdmin extends Component {
 
+    constructor(props) {
+        super(props);
 
-    return (
-        <div>HEY MATHEW </div>
-    )
+        this.state = {
+            materials: null
+        }
+    }
+
+    componentDidMount() {
+        this.fetchMaterials();
+    }
+
+    async fetchMaterials() {
+        try {
+            const materials = await axios.get("http://localhost:3030/server/api/v1/materials",
+            {
+                headers: {
+                    'content-type': 'application/json'
+                }
+            }).then((response) => {
+                return response.data;
+            }).catch((err) => {
+                console.error(err);
+                return [];
+            });
+
+            this.setState({materials})
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render(){
+        console.log(this.state.materials);
+        return (
+            <div>
+                <Homebar />
+
+                <h2>Hello, Administrator</h2>
+
+                <ListGroup>
+
+                {this.state.materials?.map(material => {
+                    return(
+                        <div>
+                            <ListGroup.Item>
+                            <Row>
+                                    <Col>{material._id}</Col>
+                                    <Col><a href="#">{material.priority_name}</a></Col>
+                                    <Col><Button variant="danger">Delete</Button>{' '}</Col>
+                            </Row>
+                            </ListGroup.Item>
+                        </div>
+                    )
+                })}
+                
+                </ListGroup>
+
+            </div>
+        )
+    }
 }
 
 export default MaterialsAdmin;
